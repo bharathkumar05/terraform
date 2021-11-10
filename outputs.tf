f4748afe1bc1b802376e9e7b1e6211a97f3bb375
bharath <bharathkumaganji@gmail.com>
2021-11-10 14:24:38 +0530

    I  am created an outputs file updated this file content

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
-output "abc" {
-  value = "hello\n bharath"
+#output "abc"
+#{
+ # value = "hello\n bharath"
+#}
+
+variable "abc"
+{
+  default = "100"
 }
+
+output "abc"
+{
+  value = var.abc
+}
+
