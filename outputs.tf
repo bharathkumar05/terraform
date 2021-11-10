4dcbc211c55025338327aa89ad9453cb328cdca6
bharath <bharathkumaganji@gmail.com>
2021-11-10 14:59:25 +0530

    I  am created an outputs file updated this file content again udating the variable list

--- outputs.tf
+++ outputs.tf
@@ -21,11 +21,11 @@ output "samplex" {
   value = "value of 1=${var.samplex[0]}, value of 2 =${var.samplex[1]}, value of 3= ${var.samplex[2]}"
 }
 variable "sampley" {
-  default = [
-  string = "hello bharath",
-  boolean = true,
-    number = 1000
-  ]
+  default = {
+    string = "hello bharath",
+    numberr = 1000,
+    boolean = true
+  }
 }
 output "sampley" {
   value = "value of string ${"string"}"
