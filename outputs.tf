900a2ba7c49fd3fff5f40da3b72fc0a340df16e4
bharath <bharathkumaganji@gmail.com>
2021-11-10 14:52:00 +0530

    I  am created an outputs file updated this file content again udating the variable list

--- outputs.tf
+++ outputs.tf
@@ -10,4 +10,13 @@ variable "abc1" {
 output "abc1" {
   value = "value of abc =${var.abc1}"
 }
-
+ variable "samplex" {
+   default = [
+   "bharath",
+     100,
+     true
+   ]
+ }
+output "samplex" {
+  value = "value of 1=${var.samplex[0]}, value of 2 =${var.samplex[1]}, value of 3= ${var.samplex[2]}"
+}
