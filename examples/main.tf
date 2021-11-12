f2e6507f561b2a9fe2397d50f565843852a4bd8e
bharath <bharathkumaganji@gmail.com>
2021-11-12 13:38:07 +0530

    I  am created an outputs file updated this file content again udating the variable list creating a example secutity group and ec2 instance from terraform

--- examples/main.tf
+++ examples/main.tf
@@ -2,23 +2,34 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_instance" "sample" {
+  ami           = "ami-0855cab4944392d0a"
+  instance_type = "t2.small"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
 resource "aws_security_group" "allow_sample" {
   name        = "allow_sample"
   description = "Allow sample traffic"
 
-  ingress {
+  ingress = [
+   {
     description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
+    from_port        = 22
+    to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = []
     prefix_list_ids  = []
     security_groups  = []
     self             = false
-  }
+   }
+  ]
 
-  egress {
+  egress =[
+   {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
@@ -27,19 +38,14 @@ resource "aws_security_group" "allow_sample" {
     prefix_list_ids  = []
     security_groups  = []
     self             = false
-  }
+   }
+  ]
 
   tags = {
     Name = "allow_sample"
   }
 }
 
-resource "aws_instance" "sample" {
-  ami           = "ami-0855cab4944392d0a"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_sample.id]
 
-  tags = {
-    Name = "sample"
-  }
-}
+
+
