16475ef9bc79c0a7b488c8cb70c2d47950fd4254
bharath <bharathkumaganji@gmail.com>
2021-11-16 14:31:21 +0530

    code updated

---
+++ examples/roboshop/main.tf
@@ -0,0 +1,20 @@
+resource "aws_spot_instance_request" "cheap_worker" {
+  count                  = length(var.components)
+  ami                    = data.aws_ami.ami.id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0ff119ffc5aabad9e "]
+  #wait_for_fulfillment   = true
+  tags = {
+    Name = element(var.components, count.index)
+  }
+}
+
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "^Cent*"
+  owners      = ["973714476881"]
+}
+
+variable "components" {
+  default = ["frontend", "mongodb", "catalogue", "cart", "user", "redis", "mysql", "shipping", "rabbitmq", "payment"]
+}
